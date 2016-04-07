e264f02dabd0c1ef0374a7a1707d53bfda6bde2e
Ubuntu <ubuntu@ip-172-31-29-249.us-west-2.compute.internal>
2016-04-07 02:14:31 +0000

    add v4

---
+++ debug_online_v4_turk.py
@@ -0,0 +1,37 @@
+#!/usr/bin/etc python
+
+import galbackend_online
+import sys
+import socket
+import time
+galbackend_online.InitLogging()
+galbackend_online.InitResource('v3')
+oov_state =1
+name_entity_state =1
+anaphra_state =1
+short_answer_state=1
+previous_history ={}
+word2vec_ranking_state =1
+tfidf_state =1
+policy_mode =1
+user_list =[]
+theme = {}
+while True:
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Log('serversocket')
+        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversocket.bind(('localhost', 13113))
+        serversocket.listen(5)
+        connection, address = serversocket.accept()
+        #Log('connection established')
+        print connection
+        user_input = connection.recv(1024)
+	print user_input
+        user_id, user_input_real = user_input.split('|')
+	print user_id
+        print user_input_real
+        theme, strategy,response,previous_history, word2vec = galbackend_online.get_response(policy_mode, user_input_real, user_id,previous_history,theme,oov_state,name_entity_state,short_answer_state,anaphra_state,word2vec_ranking_state,tfidf_state)
+        connection.send(response + "|" + str(strategy))
+        print 'finish sending response'
+        serversocket.close()
+
